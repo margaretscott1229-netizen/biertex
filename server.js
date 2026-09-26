8d0d8e634443313d50f6dc95a9dacef9babebc91
margaretscott1229-netizen <margaretscott1229@gmail.com>
2026-09-26 15:48:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ async function sendEmail({ to, toName, subject, html }) {
   await brevo.transactionalEmails.sendTransacEmail({
     subject,
     htmlContent: html,
-    sender: { name: 'Biertex', email: 'noreply@biertex.dev' },
+    sender: { name: 'Biertex', email: 'margaretscott1229@gmail.com' },
     to: [{ email: to, name: toName }]
   });
 }
